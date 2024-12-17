2632709886c8f142f3592bfe62df53cbd5dc0dcc
Nathan Rihet <50133907+NathanKneT@users.noreply.github.com>
2024-12-17 15:11:04 +0100

    build config

--- voile-storage-app/next.config.ts
+++ voile-storage-app/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',  // Important pour Netlify
+  images: {
+    unoptimized: true, // Nécessaire pour l'export statique
+  },
+  trailingSlash: true, // Optionnel mais peut aider avec les routes
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
